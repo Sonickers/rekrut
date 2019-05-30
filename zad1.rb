4f0ffcfd3bc2ca7ae049b35c391a050b51996f5f
Sonickers <soniarepec@gmail.com>
2019-05-30 11:30:06 +0200

    Solve task 1

--- zad1.rb
+++ zad1.rb
@@ -2,9 +2,9 @@ def fizz_buzz
   (1..100).each do |d|
     if d % 3 == 0 && d % 5 == 0
       puts "FizzBuzz"
-    elsif d != 3 && d % 3 == 0
+    elsif d % 3 == 0
       puts "Fizz"
-    elsif d != 5 && d % 5 == 0
+    elsif d % 5 == 0
       puts "Buzz"
     else
       puts d
